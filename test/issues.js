83400002e1f0ce9109da691eef208c7dc94dbeaf
saintelama <christoph.walcher@gmail.com>
2014-01-02 10:30:49 +0100

    Fixes error thrown in case no salt value or hashed password is stored in the mongo db collection. Closes #27

--- test/issues.js
+++ test/issues.js
@@ -41,4 +41,64 @@ describe('issues', function () {
             });
         });
     });
+
+    it('should support not throw exception in case hash or salt are not stored - Issue #27', function(done) {
+        var UserSchema = new Schema({
+            name: String,
+            age: Number
+        });
+
+        UserSchema.plugin(passportLocalMongoose);
+        var User = mongoose.model('ShouldNotThrowIfPasswordOrSaltAreNotStored_Issue_27', UserSchema);
+
+        User.remove({}, function(err) {
+            assert.ifError(err);
+
+            User.create({ username: 'hugo', name : 'Hugo Wiener', age : 143 }, function(err, user) {
+                assert.ifError(err);
+                assert.ok(user);
+
+                User.authenticate()('hugo', 'none', function(err, auth, reason) {
+                    assert.ifError(err);
+                    assert.equal(false, auth);
+                    assert.ok(reason);
+
+                    assert.equal('User cannot be logged in. No salt value stored in mongodb', reason.message);
+
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should support not throw exception in case hash and salt are not selected - Issue #27', function(done) {
+        var UserSchema = new Schema({
+            name: String,
+            age: Number
+        });
+
+        UserSchema.plugin(passportLocalMongoose, { selectFields : 'name' });
+        var User = mongoose.model('ShouldNotThrowIfPasswordAndSaltAreNotSelected_Issue_27', UserSchema);
+
+        User.remove({}, function(err) {
+            assert.ifError(err);
+
+            User.register(new User({ username : 'hugo' }), 'password', function(err, user) {
+                assert.ifError(err);
+                assert.ok(user);
+
+                var authenticate = User.authenticate();
+
+                authenticate('hugo', 'password', function(err, auth, reason) {
+                    assert.ifError(err);
+                    assert.equal(false, auth);
+                    assert.ok(reason);
+
+                    assert.equal('User cannot be logged in. No salt value stored in mongodb', reason.message);
+
+                    done();
+                });
+            });
+        });
+    });
 });
