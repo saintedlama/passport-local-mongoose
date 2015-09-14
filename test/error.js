6e0388bcb749ed434ad5fe247d2ec64181e57711
Frederic Hemberger <mail@frederic-hemberger.de>
2015-09-14 16:18:39 +0200

    Error handling: Always return instance of 'AuthenticationError'

--- test/error.js
+++ test/error.js
@@ -1,22 +1,22 @@
-var BadRequestError = require('../lib/badrequesterror');
+var Err = require('../lib/error.js');
 var expect = require('chai').expect;
 
-describe('BadRequestError', function() {
+describe('AuthenticationError', function() {
   it('should construct a valid error with stack trace and name', function() {
-    var error = new BadRequestError();
+    var error = new Err.AuthenticationError();
 
     expect(error.stack).to.exist;
-    expect(error.name).to.equal('BadRequestError');
+    expect(error.name).to.equal('AuthenticationError');
   });
 
   it('should construct a bad request error with message passed', function() {
-    var error = new BadRequestError('Test');
+    var error = new Err.AuthenticationError('Test');
 
     expect(error.message).to.equal('Test');
   });
 
   it('should construct a bad request error with null message if no message was passed', function() {
-    var error = new BadRequestError();
+    var error = new Err.AuthenticationError();
 
     expect(error.message).to.equal(null);
   });
