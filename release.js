0dc4d5cabae77a5ae5f10f831f580aaa8256d11d
saintedlama <christoph.walcher@gmail.com>
2016-06-12 15:48:56 +0200

    Fix output handling in shelljs 0.7

--- release.js
+++ release.js
@@ -1,6 +1,6 @@
 var shell = require('shelljs');
 
-if (exec('git status --porcelain').output != '') {
+if (exec('git status --porcelain').output) {
   console.error('Git working directory not clean.');
   process.exit(2);
 }
