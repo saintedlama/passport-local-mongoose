7992af4f72d6762982d85471ef422059f0cbd566
saintedlama <christoph.walcher@gmail.com>
2020-01-04 20:39:55 +0100

    chore: simplify code

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ module.exports = function(schema, options) {
   options.encoding = options.encoding || 'hex';
   options.digestAlgorithm = options.digestAlgorithm || 'sha256'; // To get a list of supported hashes use crypto.getHashes()
 
-  options.passwordValidator =
-    options.passwordValidator ||
-    function(password, cb) {
-      cb(null);
-    };
-  options.passwordValidatorAsync =
-    options.passwordValidatorAsync ||
-    function(password) {
-      return new Promise((resolve, reject) => {
-        options.passwordValidator(password, err => (err ? reject(err) : resolve()));
-      });
-    };
+  function defaultPasswordValidator(password, cb) {
+    cb(null);
+  }
+
+  function defaultPasswordValidatorAsync(password) {
+    return new Promise((resolve, reject) => {
+      options.passwordValidator(password, err => (err ? reject(err) : resolve()));
+    });
+  }
+
+  options.passwordValidator = options.passwordValidator || defaultPasswordValidator;
+  options.passwordValidatorAsync = options.passwordValidatorAsync || defaultPasswordValidatorAsync;
 
   // Populate field names with defaults if not set
   options.usernameField = options.usernameField || 'username';
