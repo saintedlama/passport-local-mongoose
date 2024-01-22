ce1dfa38303602585fb79c4e5fe1c15e512e0d50
Olawale Micheal Juwon <olawalejuwon@gmail.com>
2024-01-22 13:23:54 +0100

    add immutable to default salt and hash fields

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ module.exports = function (schema, options) {
   if (!schema.path(options.usernameField)) {
     schemaFields[options.usernameField] = { type: String, unique: options.usernameUnique };
   }
-  schemaFields[options.hashField] = { type: String, select: false };
-  schemaFields[options.saltField] = { type: String, select: false };
+  schemaFields[options.hashField] = { type: String, select: false,  immutable: true};
+  schemaFields[options.saltField] = { type: String, select: false,  immutable: true };
 
   if (options.limitAttempts) {
     schemaFields[options.attemptsField] = { type: Number, default: 0 };
